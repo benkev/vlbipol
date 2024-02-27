4e1cbea25bc0e3788c741d788732c420fae9ab34
Leonid Benkevitch <benkev@mit.edu>
2024-02-27 17:46:20 -0500

    ...

--- group_delay.py
+++ group_delay.py
@@ -4,9 +4,9 @@
 # Get multiband/group delay data before and after PolConvert 
 #
 
-hops_a = "/data-sc16/geodesy/3819/251-1130"
+hops_a = "/data-sc16/geodesy/3819/251-1130/"
 hops_b = "/data-sc16/geodesy/3819/polconvert/3819/scratch/" \
-         "pol_prods1/3819/251-1130"
+         "pol_prods1/3819/251-1130/"
 
 import mk4b
 
@@ -15,4 +15,4 @@ mf_b = mk4b.mk4fringe(hops_b + 'EV.X.5.2HMQIA')
 
 pcal = []
 for ii in range(32):
-    pcal = pcal.append(mf_a.t207.contents.ref_pcamp[ii].usb)
+    pcal.append(mf_b.t207.contents.ref_pcamp[ii].usb)
