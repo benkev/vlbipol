d5a58430abee56e7d90e6e01de1a62ba9207ac0d
Leonid Benkevitch <lbenkev@gmail.com>
2024-04-18 20:15:26 -0400

    plot_mbdelay.py excludes bad data at the beginning (2 points).

--- plot_mbdelay.py
+++ plot_mbdelay.py
@@ -26,52 +26,57 @@ bls = list(idx3819l_1.keys())   # Baselines
 bls.sort()                      # Lexigraphically sorted baselines
 nbls = len(bls)
 
-lmin = np.zeros(nbls, dtype=float)
-lmax = np.zeros(nbls, dtype=float)
-cmin = np.zeros(nbls, dtype=float)
-cmax = np.zeros(nbls, dtype=float)
-dmin = np.zeros(nbls, dtype=float)
-dmax = np.zeros(nbls, dtype=float)
+# lmin = np.zeros(nbls, dtype=float)
+# lmax = np.zeros(nbls, dtype=float)
+# cmin = np.zeros(nbls, dtype=float)
+# cmax = np.zeros(nbls, dtype=float)
+# dmin = np.zeros(nbls, dtype=float)
+# dmax = np.zeros(nbls, dtype=float)
 
-#
-# Find vertical ranges for plotting
-#
-ibl = 0
-for bl in bls:   # Loop over the baselines
-    mbd_l = np.array(idx3819l_1[bl]['I']['mbdelay'])
-    mbd_c = np.array(idx3819c_1[bl]['I']['mbdelay'])
-    lmin[ibl] = mbd_l.min()
-    lmax[ibl] = mbd_l.max()
-    cmin[ibl] = mbd_c.min()
-    cmax[ibl] = mbd_c.max()
+# #
+# # Find vertical ranges for plotting
+# #
+# ibl = 0
+# for bl in bls:   # Loop over the baselines
+#     mbd_l = np.array(idx3819l_1[bl]['I']['mbdelay'])[:-25]
+#     mbd_c = np.array(idx3819c_1[bl]['I']['mbdelay'])[:-25]
+#     lmin[ibl] = mbd_l.min()
+#     lmax[ibl] = mbd_l.max()
+#     cmin[ibl] = mbd_c.min()
+#     cmax[ibl] = mbd_c.max()
 
-    dmbd = mbd_c - mbd_l
-    dmin[ibl] = dmbd.min()
-    dmax[ibl] = dmbd.max()
+#     dmbd = mbd_c - mbd_l
+#     dmin[ibl] = dmbd.min()
+#     dmax[ibl] = dmbd.max()
 
     
-    ibl = ibl + 1
+#     ibl = ibl + 1
 
-ymin = min(lmin.min(), cmin.min())
-ymax = max(lmax.max(), cmax.max())
+# ymin = min(lmin.min(), cmin.min())
+# ymax = max(lmax.max(), cmax.max())
 
-ydmin = dmin.min()
-ydmax = dmax.max()
+# ydmin = dmin.min()
+# ydmax = dmax.max()
 
    
-# sys.exit(0)
+# # sys.exit(0)
 
 
 
 fig1 = pl.figure(1, figsize=(8, 12))
 fig2 = pl.figure(2, figsize=(8, 12))
 
+#
+# To start plotting from;  exclude bad data before istart.
+#
+istart = 2
+
 ibl = 1
 for bl in bls:   # Loop over the baselines
-    tim = np.array(idx3819l_1[bl]['I']['time']) / 60
+    tim = np.array(idx3819l_1[bl]['I']['time'])[istart:] / 60
     tim = tim - tim[0]
-    mbd_l = np.array(idx3819l_1[bl]['I']['mbdelay'])
-    mbd_c = np.array(idx3819c_1[bl]['I']['mbdelay'])
+    mbd_l = np.array(idx3819l_1[bl]['I']['mbdelay'])[istart:]
+    mbd_c = np.array(idx3819c_1[bl]['I']['mbdelay'])[istart:]
 
     pl.figure(1)
     pl.subplot(5, 3, ibl)
