afe96548904e72dcf88ce4f8775c9cef5ffb7fe6
Leonid Benkevitch <lbenkev@gmail.com>
2025-05-05 03:59:02 -0400

    In clot.py, clotr[tr][sr][di] is created from clos[sr][tr][di].

---
+++ clot.py
@@ -0,0 +1,57 @@
+import pickle, copy, sys
+import libvp
+from libvp import find_baseline_triangles
+
+with open('idx2187lI.pkl', 'rb') as finp: idxl = pickle.load(finp)
+with open('clos2187lI.pkl', 'rb') as finp: closl = pickle.load(finp)
+with open('bls_2187.pkl', 'rb') as finp: bls = pickle.load(finp)
+
+trians = find_baseline_triangles(bls)
+
+#
+# clotr[tm][sr][tr]
+#
+                
+clotr = {tr: {} for tr in trians.keys()} 
+
+for sr in closl.keys():
+    for tr in closl[sr].keys():
+        for di in closl[sr][tr].keys():
+            if sr in clotr[tr].keys():
+                clotr[tr][sr][di] = copy.deepcopy(closl[sr][tr][di])
+            else:
+                clotr[tr][sr] = {}
+                clotr[tr][sr][di] = copy.deepcopy(closl[sr][tr][di])
+
+
+sys.exit(0)
+                
+#
+# clotm[tm][sr][tr]   ??????????????????????
+#
+                
+#
+# Gather in tms a sorted list of all times in seconds
+#
+tms = set()
+for bl in idxl.keys():
+    tms = tms | set(idxl[bl]['I']['time'])
+tms = list(tms)
+tms.sort()
+
+                
+clotm = {tm: {} for tm in tms} 
+
+for sr in closl.keys():
+    for tr in closl[sr].keys():
+        for di in closl[sr][tr].keys():
+            if sr in clotm[tm].keys():
+                clotm[tr][sr][di] = copy.deepcopy(closl[sr][tr][di])
+            else:
+                clotm[tr][sr] = {}
+                clotm[tr][sr][di] = copy.deepcopy(closl[sr][tr][di])
+
+
+
+
+                
